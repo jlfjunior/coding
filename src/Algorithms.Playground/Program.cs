2ec026cf778f4fce091605b7f6a70d1929871d61
Jose Junior <jlfjunior8@gmail.com>
2023-08-13 08:31:39 -0300

    feat: add Insertion Sorting recursion version

--- src/Algorithms.Playground/Program.cs
+++ src/Algorithms.Playground/Program.cs
@@ -8,6 +8,6 @@ var numbers = new int[] { 2, 1, 4, 5, 6 };
 Console.WriteLine(numbers.PrintToString());
 Console.WriteLine("Starting sorting data...");
 
-InsertionSorting.Sorting(numbers);
+InsertionSorting.Sorting(numbers, 5);
 
 Console.WriteLine(numbers.PrintToString());
--- src/Algorithms.Playground/Sorting/InsertionSorting.cs
+++ src/Algorithms.Playground/Sorting/InsertionSorting.cs
@@ -23,4 +23,28 @@ public abstract class InsertionSorting
             list[j + 1] = element;
         }
     }
+    
+    /// <summary>
+    /// The function <c>Sorting</c> sort an array of number elements using recursion
+    /// Worst case complexity Nˆ2
+    /// </summary>
+    /// <param name="list"></param>
+    /// <param name="size"></param>
+    public static void Sorting(int[] list, int size)
+    {
+        if (size == decimal.One) return;
+        
+        Sorting(list, size - 1);
+
+        var j = size - 2;
+        var element = list[size - 1];
+
+        while (j >= Decimal.Zero && list[j] > element)
+        {
+            list[j + 1] = list[j];
+            j--;
+        }
+
+        list[j + 1] = element;
+    }
 }
