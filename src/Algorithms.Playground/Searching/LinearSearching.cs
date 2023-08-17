6a5c7f1e6efaf33e0533f37e78643153207f4f4d
Jose Junior <jlfjunior8@gmail.com>
2023-08-17 20:52:10 -0300

    feat: implement sum function

--- src/Algorithms.Playground/Searching/LinearSearching.cs
+++ src/Algorithms.Playground/Searching/LinearSearching.cs
@@ -22,4 +22,35 @@ public abstract class LinearSearching
 
         return -1;
     }
+    
+    /// <summary>
+    /// The function <c>Sum</c> sum the value of all elements on the array using for
+    /// Worst case complexity N
+    /// </summary>
+    /// <param name="list"></param>
+    public static int Sum(int[] list)
+    {
+        var sum = 0;
+        
+        for (int i = 0; i < list.Length; i++)
+        {
+            sum += list[i];
+        }
+
+        return sum;
+    }
+    
+    /// <summary>
+    /// The function <c>Sum</c> sum the value of all elements on the array using recursion
+    /// Worst case complexity N
+    /// </summary>
+    /// <param name="list"></param>
+    /// <param name="size"></param>
+    public static int Sum(int[] list, int size)
+    {
+        var value = list[size - 1];
+        var sum = value + Sum(list, size - 1);
+        
+        return sum;
+    }
 }
