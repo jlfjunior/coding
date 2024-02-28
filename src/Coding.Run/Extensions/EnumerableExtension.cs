49951cafeefa0ba8bd7fd3f28608344b3f47e804
Jose Junior <jlfjunior8@gmail.com>
2024-02-27 21:28:19 -0300

    feat: remove console application project

--- src/Coding.Run/Extensions/EnumerableExtension.cs
+++
@@ -1,9 +0,0 @@
-namespace Coding.Run.Extensions;
-
-public static class EnumerableExtension
-{
-    public static string PrintToString(this int[] array)
-    {
-        return $"[ {string.Join(", ", array)} ]";
-    }
-}
